6190fe3f2983d324a9abe0bd562ef07e45770545
Ricardo Sperlongo DECSKILL <RicardoSperlongo.DECSKILL@edp.com>
2022-05-27 15:40:31 -0300

    added usersservice in authmodule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,23 @@
+import { UsersService } from 'src/users/users.service';
+import { JwtStrategy } from './jwt.strategy';
+import { LocalStrategy } from './local-strategy';
+import { jwtConstants } from './constant';
+import { UsersModule } from './../users/users.module';
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  providers: [AuthService]
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy, UsersService],
+  exports: [AuthService],
 })
 export class AuthModule {}
